d88abeca0143cc55cd5c32b503402e3c3e88275d
guilherme2020 <guilhermerodrigues123477@gmail.com>
2019-02-13 19:30:10 -0300

    changues

--- functions.sql
+++ functions.sql
@@ -82,12 +82,84 @@ returns void as $$
 $$ language plpgsql;
 
 
+--create or replace function inserir_salario_mes(funcionario_id int, mes_id int)
+--returns void as $$
+--	declare
+--		funcionario_id int;
+--		mes_id int;
+--	begin 
+--			select max(id) into funcionario_id  from funcionario;
+--			select max(id) into mes_id from mes;
+--
+--			if(funcionario_id is null and mes_id) then
+--				funcionario_id  := 1;
+--				mes_id := 1;
+--			else
+--				funcionario_id  := funcionario_id +1;
+--				mes_id := mes_id + 1;
+--			end if;
+--			if($1 is null) then 
+--				raise exception 'preencha o salario mes';
+--			else
+--				insert into salario_mes values(funcionario_id,mes_id,salario);
+--	end 
+--
+--$$ language plpgsql;
+create function inserir_tipo_pagamento(descricao varchar)
+returns void as $$
+	
+	begin 
+		if ($1 isnull or  $1 like '') then
+			raise exception 'descricao não pode ser nulo';
+		else
+			insert into tipo_pagamento values (descricao);
+		end if;
+	end 
+	
+$$ language plpgsql;
 
 
 
+--CREATE FUNCTION CALCULAR_SALARIO_FUNCIONARIO(FUNCIONARIO_ID INT , MES_ID INT ) RETURNS FLOAT AS $$
+    -- PARA ESSE MÊS, CALCULAR  SALÁRIO DO FUNCIONÁRIO
+            -- AQUI FAZ A VERIFICAÇÃO DO QUANTO O FUNCIONARIO VENDEU ATRAVÉS DOS PEDIDOS
+        -- SELECIONAR OS PEDIDOS, VERIFICAR O PREÇO TOTAL * QUANTIDADE + SALARIO_FUNCIONARIO
+
+create or replace function aplicar_bonificacao_para_o_vendedor_do_mes (mes_id int ) 
+	returns float as $$
+    DECLARE 
+	
+        QTD_VENDAS_FUNC  int := 0;
+        SALARIO_ATUALIZADO float := 0;
+		porcentagem float := 0.15;
+		query float;
+
+    BEGIN
+
+		query :=  "select funcionario_id,valor_total from pedido  group by sum(valor_total)";
+		execute query;
+		SALARIO_ATUALIZADO := (SALARIO_ATUALIZADO +  query) *  porcentagem;
+        RETURN SALARIO_ATUALIZADO;
+    END;
+
+$$ LANGUAGE PLPGSQL;
 
 
+CREATE FUNCTION PAGAR_FUNCIONARIO(FUNCIONARIO_ID INT , MES_ID INT) RETURNS FLOAT AS $$
+    
+    DECLARE 
 
+        SALARIO_ATUALIZADO float := 0;
+
+    BEGIN
+
+        -- PODENDO SER TANTO O SALÁRIO ORIGINAL ( CASO N TENHA VENDIDO NADA, QUANDO O INCREMENTADO )
+        SALARIO_ATUALIZADO = CALCULAR_SALARIO_FUNCIONARIO(FUNCIONARIO_ID, MES_ID); 
+
+        INSERT INTO SALARIO_MES VALUES ( FUNCIONARIO_ID, MES_ID, SALARIO_ATUALIZADO);
+    END;
+
+$$ LANGUAGE PLPGSQL;
 
 
 
